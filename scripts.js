3d360a25578f12e4f087668b634e401c430bf995
Walter Wojnarowski <walterwojnarowski@hotmail.com>
2022-11-22 15:03:02 +0000

    changed classes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
-const button = document.querySelectorAll()
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement){
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+  }
+
+  clear() {
+    
+  }
+}
+  
+const numberButtons = document.querySelectorAll(".number")
+const operationButtons = document.querySelectorAll(".function")
+const comaSign = document.querySelector(".coma")
+const equalSign = document.querySelector(".equal")
+const clearAll = document.querySelector(".clear")
+const previousOperandTextElement = document.querySelector(".previous-operand")
+const currentOperandTextElement = document.querySelector(".current-operand")
+
+
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+
+
+
+
+
+
+
+
