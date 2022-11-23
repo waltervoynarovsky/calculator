030c9ce43ec88cf2591a987034df5ffea19932c9
Walter Wojnarowski <walterwojnarowski@hotmail.com>
2022-11-23 17:01:17 +0000

    fixed multiplication, minor bug.

--- scripts.js
+++ scripts.js
@@ -38,7 +38,7 @@ class Calculator {
       case "-":
         computation = prev - current;
         break;
-      case "*":
+      case "x":
         computation = prev * current;
         break;
       case "÷":
